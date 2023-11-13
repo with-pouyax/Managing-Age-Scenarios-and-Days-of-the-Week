9697e5be5a8ef1d85f9e2f32592efe77c10b2009
pouyax-experience <75066687+with-pouyax@users.noreply.github.com>
2023-11-13 16:46:55 +0100

    Create weeklySchedule.js

---
+++ weeklySchedule.js
@@ -0,0 +1,27 @@
+var day = "sunday";
+
+switch(place){
+    case 'Monday' :
+    console.log("Do something")
+    break;
+    case 'Tuesday' :
+    console.log("Do something")
+    break;
+    case 'Wednesday' :
+    console.log("Do something")
+    break;
+    case 'Thursday' :
+    console.log("Do something")
+    case 'Friday' :
+    console.log("Do something")
+    break;
+    case 'Saturday' :
+    console.log("Do something")
+    break;
+    case 'Sunday' :
+    console.log("Do something")
+    default:
+        console.log("There is no such day")
+
+
+}
